7bcb8f201081608c5f003a7ec5794459272989ca
Tejas Sameera <ts880@ilab1.cs.rutgers.edu>
2020-04-10 15:54:44 -0400

    h

---
+++ wtfClient.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include <stdbool.h> 
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h> 
+
+int main(int argc, char* argv[]){
+
+    return 0;
+
+}
