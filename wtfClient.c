3502b3391c6e2167c52c87f1c4ce98cab942b4d4
Tejas Sameera <ts880@ilab1.cs.rutgers.edu>
2020-04-10 16:34:25 -0400

    h

--- wtfClient.c
+++ wtfClient.c
@@ -12,14 +12,10 @@
 void connectToServer(){
 
 
-<<<<<<< HEAD
 }
 
 int main(int argc, char* argv[]){
     connectToServer();
-    return 0;
-=======
     return 1;
->>>>>>> eee152cd444f1840115890bcb46527fe703cfc9d
 
 }
