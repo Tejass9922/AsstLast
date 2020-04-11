c7598632ac7dc2cf85540db13a642821ba36238a
shravan1999 <shravanpatel1999@gmail.com>
2020-04-10 23:09:05 -0400

    Command line port number

--- wtfClient.c
+++ wtfClient.c
@@ -30,7 +30,9 @@ int main(int argc, char **argv)
 	printf("Created Socket \n");
    bzero(&servaddr,sizeof (servaddr));
    servaddr.sin_family = AF_INET;
-   servaddr.sin_port = htons(6000);
+   //shrav
+   int port = atoi(argv[1]);
+   servaddr.sin_port = htons(port);
    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
    //inet_pton(AF_INET, argv[1],&servaddr.sin_addr);
    
